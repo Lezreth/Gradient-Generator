ba1498be528e5703c7ff4f8034cf5377ff8d19df
Lezreth Senceura <lezreth@yahoo.com>
2020-05-11 23:03:32 -0400

    Pre-Alpha

---
+++ Gradient Maker/FrmMain.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Gradient_Maker
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TableMain = new System.Windows.Forms.TableLayoutPanel();
+            this.ListColors = new System.Windows.Forms.ListBox();
+            this.BtnGenerateColors = new System.Windows.Forms.Button();
+            this.PictPreview = new System.Windows.Forms.PictureBox();
+            this.BtnAddColor = new System.Windows.Forms.Button();
+            this.TableMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TableMain
+            // 
+            this.TableMain.ColumnCount = 2;
+            this.TableMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TableMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TableMain.Controls.Add(this.ListColors, 0, 2);
+            this.TableMain.Controls.Add(this.BtnGenerateColors, 1, 2);
+            this.TableMain.Controls.Add(this.PictPreview, 0, 3);
+            this.TableMain.Controls.Add(this.BtnAddColor, 0, 1);
+            this.TableMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TableMain.Location = new System.Drawing.Point(0, 0);
+            this.TableMain.Name = "TableMain";
+            this.TableMain.RowCount = 4;
+            this.TableMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.TableMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.TableMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.TableMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.TableMain.Size = new System.Drawing.Size(800, 450);
+            this.TableMain.TabIndex = 0;
+            // 
+            // ListColors
+            // 
+            this.ListColors.FormattingEnabled = true;
+            this.ListColors.IntegralHeight = false;
+            this.ListColors.Location = new System.Drawing.Point(3, 227);
+            this.ListColors.Name = "ListColors";
+            this.ListColors.Size = new System.Drawing.Size(120, 96);
+            this.ListColors.TabIndex = 0;
+            // 
+            // BtnGenerateColors
+            // 
+            this.BtnGenerateColors.Location = new System.Drawing.Point(403, 227);
+            this.BtnGenerateColors.Name = "BtnGenerateColors";
+            this.BtnGenerateColors.Size = new System.Drawing.Size(75, 23);
+            this.BtnGenerateColors.TabIndex = 1;
+            this.BtnGenerateColors.Text = "Generate";
+            this.BtnGenerateColors.UseVisualStyleBackColor = true;
+            this.BtnGenerateColors.Click += new System.EventHandler(this.BtnGenerateColors_Click);
+            // 
+            // PictPreview
+            // 
+            this.PictPreview.Location = new System.Drawing.Point(3, 339);
+            this.PictPreview.Name = "PictPreview";
+            this.PictPreview.Size = new System.Drawing.Size(100, 50);
+            this.PictPreview.TabIndex = 2;
+            this.PictPreview.TabStop = false;
+            // 
+            // BtnAddColor
+            // 
+            this.BtnAddColor.Location = new System.Drawing.Point(3, 115);
+            this.BtnAddColor.Name = "BtnAddColor";
+            this.BtnAddColor.Size = new System.Drawing.Size(75, 23);
+            this.BtnAddColor.TabIndex = 3;
+            this.BtnAddColor.Text = "Add Color";
+            this.BtnAddColor.UseVisualStyleBackColor = true;
+            this.BtnAddColor.Click += new System.EventHandler(this.BtnAddColor_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TableMain);
+            this.Name = "FrmMain";
+            this.Text = "Gradient Generator";
+            this.TableMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PictPreview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel TableMain;
+        private System.Windows.Forms.ListBox ListColors;
+        private System.Windows.Forms.Button BtnGenerateColors;
+        private System.Windows.Forms.PictureBox PictPreview;
+        private System.Windows.Forms.Button BtnAddColor;
+    }
+}
+
---
+++ Gradient Maker/FrmMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gradient_Maker
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void GenerateGradient(Color Color1, Color Color2, int GradientSize)
+        {
+            int rMax = Color1.R;
+            int rMin = Color2.R;
+            int gMax = Color1.G;
+            int gMin = Color2.G;
+            int bMax = Color1.B;
+            int bMin = Color2.B;
+
+            List<Color> ColorList = new List<Color>();
+            for (int i = 0; i < GradientSize; i++)
+            {
+                int rAverage = rMin + ((rMax - rMin) * i / GradientSize);
+                int gAverage = gMin + ((gMax - gMin) * i / GradientSize);
+                int bAverage = bMin + ((bMax - bMin) * i / GradientSize);
+                ColorList.Add(Color.FromArgb(rAverage, gAverage, bAverage));
+            }
+        }
+
+        private void BtnGenerateColors_Click(object sender, EventArgs e)
+        {
+            GenerateGradient();
+        }
+
+        private void BtnAddColor_Click(object sender, EventArgs e)
+        {
+            using ColorDialog dialog = new ColorDialog();
+
+        }
+    }
+}
